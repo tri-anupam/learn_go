54a6ad43d1a023663f1ef3905742a7335a0b42d6
Anupam Tripathi <arvindtri.70@gmail.com>
2022-10-06 17:45:57 +0530

    Use interfaces concept

--- 13interfaces/main.go
+++ 13interfaces/main.go
@@ -1,11 +1,25 @@
 package main
 
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
 func main() {
 	//interfaces
+	eb := englishBot{}
+	sb := spanishBot{}
 
+	printGreetng(eb)
+	printGreetng(sb)
+}
+
+func printGreetng(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
 func (eb englishBot) getGreeting() string {
