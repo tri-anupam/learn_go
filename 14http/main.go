19c8ebfdeacbb0ed5bfcc925ef585671246b4d33
Anupam Tripathi <arvindtri.70@gmail.com>
2022-10-07 02:21:23 +0530

    interface with io package and http package

--- 14http/main.go
+++ 14http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -13,5 +16,19 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	// fmt.Println(resp)
+
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bites: ", len(bs))
+	return len(bs), nil
 }
