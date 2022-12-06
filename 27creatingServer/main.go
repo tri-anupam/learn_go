0c622e0e829fa982b35b418aaa4a7ea06b193f82
Anupam Tripathi <arvindtri.70@gmail.com>
2022-12-06 20:54:26 +0530

    perform post request

--- 27creatingServer/main.go
+++ 27creatingServer/main.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Welcome to GOO")
-	PerformGetRequest()
+	fmt.Println("Welcome to GOooooo")
+	//PerformGetRequest()            //to perform get request
+
+	PerformPostRequest() //to perform post request
+
 }
 
 func PerformGetRequest() {
@@ -26,5 +30,27 @@ func PerformGetRequest() {
 
 	content, _ := ioutil.ReadAll(response.Body)
 
+	fmt.Println(string(content))
+}
+
+func PerformPostRequest() {
+	const myurl = "http://localhost:8000/post"
+
+	// fake json paylaod
+
+	requestBody := strings.NewReader(`
+		{
+			"coursename":"Let's go with golang",
+			"price":0,
+			"platform":"www.google.com"
+		}
+	`)
+
+	response, err := http.Post(myurl, "application/json", requestBody)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	content, _ := ioutil.ReadAll(response.Body)
 	fmt.Println(string(content))
 }
