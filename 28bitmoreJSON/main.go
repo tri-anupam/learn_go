6135a1bb0a6b92ca9114867fa1d5733f2adeede0
Anupam Tripathi <arvindtri.70@gmail.com>
2022-12-07 21:29:23 +0530

    working with json in golang

--- 28bitmoreJSON/main.go
+++ 28bitmoreJSON/main.go
@@ -6,16 +6,17 @@ import (
 )
 
 type course struct {
-	Name     string
+	Name     string `json:"coursename"`
 	Price    int
 	Platform string
-	Password string
-	Tags     []string
+	Password string   `json:"-"`              //'-' dash don't reflect the value to the user but it stores it.
+	Tags     []string `json:"tags,omitempty"` //'omitempty don't reflect the field which is empty.'
 }
 
 func main() {
 	fmt.Println("Welcome to JSON")
-	EncodeJson()
+	// EncodeJson()
+	DecodeJson()
 }
 
 func EncodeJson() {
@@ -27,10 +28,46 @@ func EncodeJson() {
 
 	//package this data as JSON data
 
-	finalJson, err := json.Marshal(allCourses)
+	finalJson, err := json.MarshalIndent(allCourses, "", "\t")
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Printf("%s\n", finalJson)
+}
+
+//how to consume JSON data in golang
+
+func DecodeJson() {
+	jsonDataFromWeb := []byte(`
+	{
+		"coursename": "ReactJS Bootcamp",
+		"Price": 299,
+		"Platform": "Youtube",
+		"tags": ["web-dev","js"]
+	}
+	`)
+
+	var yourCourse course
+
+	checkValid := json.Valid(jsonDataFromWeb) //give the boolean value
+
+	if checkValid {
+		fmt.Println("Json was valid")
+		json.Unmarshal(jsonDataFromWeb, &yourCourse) //'&' with yourCourse gave the reference to it.
+		fmt.Printf("%#v\n", yourCourse)
+	} else {
+		fmt.Println("your json is not valid")
+	}
+
+	//some cases where you want to add to kay value
+
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	fmt.Printf("%#v\n", myOnlineData)
+
+	for k, v := range myOnlineData {
+		fmt.Printf("key is %v and value is %v and type is: %T\n", k, v, v)
+	}
+
 }
